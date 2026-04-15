b14fe564b34f835fb661056b1d6b15e86d1a57d3
Garfunkel123 <demigarfu@gmail.com>
2026-04-15 19:28:57 -0300

    merge con cambios del profe

--- Unidad_5/_2_actividad_1.py
+++ Unidad_5/_2_actividad_1.py
@@ -9,3 +9,22 @@ Se nos solicita crear un bloque de código que realice lo siguiente:
 5. Si es inválido, mostrar un mensaje en la consola
 6. A fin de poder repetir el flujo, incluir el código dentro de un bucle while
 """
+
+
+estudiantes = []
+
+while True:
+    print("BIENVENIDO...")
+    nombre = input("ingrese su nombre: ")
+
+    if nombre.strip() != "":
+        print("TRUE")
+        estudiantes.append(nombre)
+    else:
+        print("Nombre no puede quedar en blanco")
+
+
+    flag = input("Ingrese 's' para terminar")
+    if flag == "s":
+        break
+
